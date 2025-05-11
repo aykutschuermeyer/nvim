ad7aab13fd0fef65f298039a54a059c88da64346
Aykut Schürmeyer <aykutschuermeyer@pm.me>
2025-05-11 11:23:13 +0200

    Re-enable arrow keys for easier oil navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,9 +3,3 @@ vim.keymap.set("n", "<tab>", "<CMD>bnext<CR>", {desc="Next buffer"})
 vim.keymap.set("n", "<S-tab>", "<CMD>bprev<CR>", {desc="Previous buffer"})
 vim.keymap.set("n", "<leader>x", "<CMD>bd<CR>", {desc="Close buffer"})
 
--- Disable arrow keys in normal mode
-vim.keymap.set("n", "<left>", "<cmd>echo 'Use h to move!'<CR>")
-vim.keymap.set("n", "<right>", "<cmd>echo 'Use l to move!'<CR>")
-vim.keymap.set("n", "<up>", "<cmd>echo 'Use k to move!'<CR>")
-vim.keymap.set("n", "<down>", "<cmd>echo 'Use j to move!'<CR>")
-
