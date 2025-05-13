1a2c4bacb5a52a15d6fb16a131ccf41ae41be050
Aykut Schürmeyer <aykutschuermeyer@pm.me>
2025-05-13 06:02:09 +0200

    Configure diagnostics, inline error messages

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -7,12 +7,12 @@ return {
   opts = {},
   keys = {
     {
-      "<leader>ff", 
+      "<leader>ff",
       function() require("fzf-lua").files() end,
       desc="Find files in current directory",
     },
     {
-      "<leader>fc", 
+      "<leader>fc",
       function() require("fzf-lua").files({cwd=vim.fn.stdpath("config")}) end,
       desc="Find files in config directory",
     },
@@ -28,4 +28,3 @@ return {
     },
   }
 }
-
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -79,7 +79,7 @@ return {
     vim.diagnostic.config({
       severity_sort = true,
       float = { border = "rounded", source = "if_many" },
-      underline = { severity = vim.diagnostic.severity.ERROR },
+      underline = { severity = vim.diagnostic.severity.HINT},
       signs = {
         text = {
           [vim.diagnostic.severity.ERROR] = "󰅚 ",
@@ -94,9 +94,9 @@ return {
         format = function(diagnostic)
           local diagnostic_message = {
             [vim.diagnostic.severity.ERROR] = diagnostic.message,
-            [vim.diagnostic.severity.WARN] = diagnostic.message,
-            [vim.diagnostic.severity.INFO] = diagnostic.message,
-            [vim.diagnostic.severity.HINT] = diagnostic.message,
+            -- [vim.diagnostic.severity.WARN] = diagnostic.message,
+            -- [vim.diagnostic.severity.INFO] = diagnostic.message,
+            -- [vim.diagnostic.severity.HINT] = diagnostic.message,
           }
           return diagnostic_message[diagnostic.severity]
         end,
