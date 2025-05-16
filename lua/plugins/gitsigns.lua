d549fed6bf78d950114690146bd70184725f668d
Aykut Schürmeyer <aykutschuermeyer@pm.me>
2025-05-16 05:40:04 +0200

    Add some git shortcuts

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,3 +1,21 @@
 return {
   "lewis6991/gitsigns.nvim",
+  lazy = false,
+  keys = {
+    {
+      "<leader>dd",
+      function() vim.cmd("Gitsigns preview_hunk") end,
+      desc="Show git difference for hunk",
+    },
+    {
+      "<leader>dr",
+      function() vim.cmd("Gitsigns reset_hunk") end,
+      desc="Rollback hunk",
+    },
+    {
+      "<leader>db",
+      function() vim.cmd("Gitsigns diffthis") end,
+      desc="Show git difference for buffer",
+    },
+  },
 }
