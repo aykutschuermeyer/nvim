31a15901fffa939ec7eafacc812b50ffe05b4a7e
Aykut Schürmeyer <aykutschuermeyer@pm.me>
2025-05-24 10:00:12 +0200

    Add keybindings for copilot inline suggestions

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,5 +1,21 @@
 return {
   "github/copilot.vim",
   lazy = true,
-  cmd = "Copilot",
+  keys = {
+    {
+      "<leader>cc",
+      "<cmd>Copilot<cr>",
+      desc = "Copilot load",
+    },
+    {
+      "<leader>ce",
+      "<cmd>Copilot enable<cr>",
+      desc = "Copilot enable inline suggestions",
+    },
+    {
+      "<leader>cd",
+      "<cmd>Copilot disable<cr>",
+      desc = "Copilot disable inline suggestions",
+    },
+  },
 }
