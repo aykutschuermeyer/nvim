d7792c6614882c82ce3a082358bb6b4dcd1152a8
Aykut Schürmeyer <aykutschuermeyer@pm.me>
2025-05-24 06:58:46 +0200

    Add more copilot keybindings

--- lua/plugins/copilotchat.lua
+++ lua/plugins/copilotchat.lua
@@ -21,10 +21,46 @@ return {
       desc = "CopilotChat stop chat",
     },
     {
-      "<leader>pr",
+      "<leader>pR",
       "<cmd>CopilotChatReset<cr>",
       mode = "n",
       desc = "CopilotChat clear buffer and chat history",
+    },
+    {
+      "<leader>pe",
+      "<cmd>CopilotChatExplain<cr>",
+      mode = "v",
+      desc = "CopilotChat explain selection",
+    },
+    {
+      "<leader>pf",
+      "<cmd>CopilotChatFix<cr>",
+      mode = "v",
+      desc = "CopilotChat fix selection",
+    },
+    {
+      "<leader>pr",
+      "<cmd>CopilotChatReview<cr>",
+      mode = "v",
+      desc = "CopilotChat review selection",
+    },
+    {
+      "<leader>po",
+      "<cmd>CopilotChatOptimize<cr>",
+      mode = "v",
+      desc = "CopilotChat optimize selection",
+    },
+    {
+      "<leader>pd",
+      "<cmd>CopilotChatDocs<cr>",
+      mode = "v",
+      desc = "CopilotChat document selection",
+    },
+    {
+      "<leader>pt",
+      "<cmd>CopilotChatTests<cr>",
+      mode = "v",
+      desc = "CopilotChat generate tests for selection",
     },
   },
 }
