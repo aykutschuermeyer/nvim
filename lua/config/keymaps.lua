c8520a6be68174b2494864c301b987fcc99315ed
Aykut Schürmeyer <aykutschuermeyer@pm.me>
2025-05-25 08:18:02 +0200

    Add keybinding to toggle relative line numbers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,7 @@
 vim.keymap.set("n", "<leader><tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 vim.keymap.set("n", "<S-tab>", "<cmd>bprev<cr>", { desc = "Previous buffer" })
 vim.keymap.set("n", "<leader>x", "<cmd>bd<cr>", { desc = "Close buffer" })
+
+vim.keymap.set("n", "<leader>rn", function()
+  vim.wo.relativenumber = not vim.wo.relativenumber
+end, { desc = "Toggle relative line numbers", noremap = true, silent = true })
